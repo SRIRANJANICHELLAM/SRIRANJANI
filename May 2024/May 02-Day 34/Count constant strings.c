326bf3592b1ff535c64288b329f8c95248e6abb1
SRIRANJANICHELLAM <157298737+SRIRANJANICHELLAM@users.noreply.github.com>
2024-05-05 23:06:13 +0530

    Create Count constant strings.c

---
+++ May 2024/May 02-Day 34/Count constant strings.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<ctype.h>
+int main()
+{
+    char s[50];
+    scanf("%s",s);
+    int i,count;
+    for(i=0;s[i]!='\0';i++) 
+    {
+        s[i]=tolower(s[i]); 
+    }
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u')
+        {
+            count++;
+        }
+        
+    }
+    printf("%d",count);
+}
+
+/*
+output
+character
+3
+*/
